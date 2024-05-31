df18d9a7a525aaeaa043d80714aaaf66182676ab
Gustave <gustave.bwele@lepoles.org>
2024-05-31 16:47:32 +0200

    php

--- pages/texte.php
+++ pages/texte.php
@@ -1,6 +1,16 @@
 <?php include  "../public/header.html.php"; ?>
 <main>
-texte
+
+
+
+
+
+    <h1>texte</h1>
+
+
+    <h3> Titre de la première section</h3>
+
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus hic ad vero, <p>architecto, sed deleniti consectetur assumenda quis totam reiciendis, cumque ullam incidunt aperiam asperiores fugiat aut laborum nesciunt quidem!</p>
 </main>
 
 <?php include "../public/footer.html.php"; ?>
